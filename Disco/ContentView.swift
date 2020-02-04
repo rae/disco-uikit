3863e71d1a778750f7698657d354e1d0fe2324a0
Reid Ellis <rae@tnir.org>
2020-02-04 00:53:42 -0500

    Added drag-and-drop to view

--- Disco/ContentView.swift
+++ Disco/ContentView.swift
@@ -8,13 +8,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View, DropDelegate {
     var body: some View {
+		Group {
         Text("Hello, World!")
             .frame(maxWidth: .infinity, maxHeight: .infinity)
+		}.onDrop(of: [""], delegate: self)
     }
-}
 
+	func dropUpdated(info: DropInfo) -> DropProposal? {
+	  let proposal = DropProposal.init(operation: .copy)
+	  return proposal
+	}
+
+	func performDrop(info: DropInfo) -> Bool {
+		print("perform drop")
+		info.itemProviders(for: [(kUTTypeFileURL as String)]).forEach { itemProvider in
+			itemProvider.loadItem(forTypeIdentifier: (kUTTypeFileURL as String), options: nil) { item, error in
+				guard let data = item as? Data, let url = URL(dataRepresentation: data, relativeTo: nil)
+				else { return }
+				// Do something with the file url
+				// remember to dispatch on main in case of a @State change
+				print("dropping \"\(url.absoluteString)\"")
+			}
+		}
+
+		return true
+	}
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
